8b3c9f5d31ec31cf6f87f0d35a15b2c38a15c9c2
Salmaan Saeed <salsaeed999@gmail.com>
2026-04-05 19:11:25 -0400

    made the search program a long running program until the user quits the program

--- main.py
+++ main.py
@@ -2,44 +2,48 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def main():
-    search_query: str = input("Search query: ")
-    df = pd.read_csv("data/extracted_urls.csv")
-    websites = {"title": [], "url": [], "description": []}
+    while True:
+        search_query: str = input("Search query (Quit with q!): ")
+        df = pd.read_csv("data/extracted_urls.csv")
+        websites = {"title": [], "url": [], "description": []}
 
-    for i in range(len(df["title"])):
-        title: str = df["title"].iloc[i]
-        description: str = df["description"].iloc[i]
-        html_path: str = df["html_path"].iloc[i]
-        url: str = df["url"].iloc[i]
+        if search_query == "q!":
+            break
 
-        if type(title) == str:
-            if search_query.lower() in title.lower():
-                websites["title"].append(title)
-                websites["url"].append(url)
-                websites["description"].append(description)
-        elif type(title) != str:
-            title = "Untitled"
-        elif type(description) == str:
-            if search_query.lower() in description.lower():
-                websites["title"].append(title)
-                websites["url"].append(url)
-                websites["description"].append(description)
-        else:
-            with open(html_path, "r", encoding='utf-8') as f:
-                html = f.read()
-                soup = BeautifulSoup(html, 'lxml')
+        for i in range(len(df["title"])):
+            title: str = df["title"].iloc[i]
+            description: str = df["description"].iloc[i]
+            html_path: str = df["html_path"].iloc[i]
+            url: str = df["url"].iloc[i]
 
-                if soup.body is not None:
-                    result = soup.body.find(string=search_query.lower())
-                    if result is not None:
-                        websites["title"].append(title)
-                        websites["url"].append(url)
-                        websites["description"].append(description)
+            if type(title) == str:
+                if search_query.lower() in title.lower():
+                    websites["title"].append(title)
+                    websites["url"].append(url)
+                    websites["description"].append(description)
+            elif type(title) != str:
+                title = "Untitled"
+            elif type(description) == str:
+                if search_query.lower() in description.lower():
+                    websites["title"].append(title)
+                    websites["url"].append(url)
+                    websites["description"].append(description)
+            else:
+                with open(html_path, "r", encoding='utf-8') as f:
+                    html = f.read()
+                    soup = BeautifulSoup(html, 'lxml')
 
-    for i in range(len(websites["title"])):
-        print(f"Title: {websites["title"][i]}")
-        print(f"URL: {websites["url"][i]}")
-        print(f"Description: {websites["description"][i]}\n")
+                    if soup.body is not None:
+                        result = soup.body.find(string=search_query.lower())
+                        if result is not None:
+                            websites["title"].append(title)
+                            websites["url"].append(url)
+                            websites["description"].append(description)
+
+        for i in range(len(websites["title"])):
+            print(f"Title: {websites["title"][i]}")
+            print(f"URL: {websites["url"][i]}")
+            print(f"Description: {websites["description"][i]}\n")
 
 if __name__ == "__main__":
     main()
